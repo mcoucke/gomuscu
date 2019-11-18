7f4b4f946d1b3baf5482fdb5af84fa4b3b79c0d6
Maxime Coucke <maxime.coucke@etu.univ-orleans.fr>
2019-11-18 19:17:12 +0100

    adding main menu buttons and statistics

--- app/src/main/java/com/app/gomuscu/MainActivity.java
+++ app/src/main/java/com/app/gomuscu/MainActivity.java
@@ -3,6 +3,7 @@ package com.app.gomuscu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickEditPlanning(View view) {
+    }
+
+    public void onClickEditSeances(View view) {
+    }
+
+    public void onClickViewHistorique(View view) {
     }
 }
---
+++ app/src/main/java/com/app/gomuscu/StatsFragment.java
@@ -0,0 +1,29 @@
+package com.app.gomuscu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class StatsFragment extends Fragment {
+
+    public StatsFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        return view;
+    }
+}
