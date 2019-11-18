64419928b7cdfff8a85f63d7ea2700eb6ca3626f
Pierre-Antoine CHEVALIER <pierre.antoine18@yahoo.fr>
2019-11-18 22:04:04 +0100

    Création de l'activité EditerSeance

---
+++ app/src/main/java/com/app/gomuscu/EditerSeance.java
@@ -0,0 +1,14 @@
+package com.app.gomuscu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditerSeance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editer_seance);
+    }
+}
