d6bca8ce087372fa7959b66b4f86863d03822bd1
Pierre-Antoine CHEVALIER <pierre.antoine18@yahoo.fr>
2019-11-19 14:41:13 +0100

    Ajout bouton 'Annuler' et 'Sauvegarder' dans CreerSeance

--- app/src/main/java/com/app/gomuscu/CreerSeance.java
+++ app/src/main/java/com/app/gomuscu/CreerSeance.java
@@ -3,6 +3,7 @@ package com.app.gomuscu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CreerSeance extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class CreerSeance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.creer_seance);
+    }
+
+    public void onClickSauvegarder(View view){
+
+    }
+
+    public void onClickAnnuler(View view){
+        finish();
     }
 }
