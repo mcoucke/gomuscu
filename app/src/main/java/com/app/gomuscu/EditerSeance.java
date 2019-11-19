2483ab6cdc91c54bbd25ff02c0885eda9df0be39
Pierre-Antoine CHEVALIER <pierre.antoine18@yahoo.fr>
2019-11-19 14:28:22 +0100

    Ajout du bouton retour au menu dans la vue editer seance

--- app/src/main/java/com/app/gomuscu/EditerSeance.java
+++ app/src/main/java/com/app/gomuscu/EditerSeance.java
@@ -3,6 +3,7 @@ package com.app.gomuscu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class EditerSeance extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class EditerSeance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editer_seance);
+    }
+
+    public void onClickCreerSeance(View view){
+
+    }
+
+    public void onClickRetourMenu(View view){
+        finish();
     }
 }
