4f83fb25d51e04d19cebca3f33e60a318a0d351f
Pierre-Antoine CHEVALIER <pierre.antoine18@yahoo.fr>
2019-11-20 18:04:56 +0100

    Ajout fragment seance pour afficher la liste des seances dans EditSeance

---
+++ app/src/main/java/com/app/gomuscu/SeancesFragment.java
@@ -0,0 +1,24 @@
+package com.app.gomuscu;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SeancesFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_seances, container, false);
+
+        return view;
+    }
+}
