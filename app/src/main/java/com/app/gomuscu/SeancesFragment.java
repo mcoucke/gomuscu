beb8b1a43b0070a8647f7968027dfe68cf3a105e
Pierre-Antoine CHEVALIER <pierre.antoine18@yahoo.fr>
2019-12-02 18:49:52 +0100

    Affichage des seances crées dans EditerSeance

--- app/src/main/java/com/app/gomuscu/SeancesFragment.java
+++ app/src/main/java/com/app/gomuscu/SeancesFragment.java
@@ -6,8 +6,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.app.gomuscu.entity.Seance;
 
 public class SeancesFragment extends Fragment {
+
+    private Seance seance;
+
+    public SeancesFragment(Seance seance){
+        this.seance = seance;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,9 @@ public class SeancesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_seances, container, false);
+
+        TextView nomSeance = view.findViewById(R.id.tv_nom_seance);
+        nomSeance.setText(seance.getNom());
 
         return view;
     }
